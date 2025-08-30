0592a117eb4f23cd4db38a6110274cd2d7e98034
aryabhattaslair <121296192+aryabhattaslair@users.noreply.github.com>
2025-08-30 12:52:38 +0000

    Adding mergesort

--- algorithm_impl.h
+++ algorithm_impl.h
@@ -2,3 +2,5 @@
 #include <vector>
 
 int binary_search(const std::vector<int> &sorted_vector, int target);
+
+std::vector<float> mergesort(const std::vector<float> &unsorted_vector);
--- main.cpp
+++ main.cpp
@@ -7,7 +7,7 @@ using namespace std;
 int main() {
   std::vector<float> sorted_vector = {4.0, 1.0, 0.0, -1.0, 5.0, 9.0, 2.0, 10.0};
   // int target = 10;
-  auto sorted_vec = mergesort(sorted_vector);
+  std::vector<float> sorted_vec = mergesort(sorted_vector);
   for (float value : sorted_vec) {
     std::cout << value << "," << std::endl;
   }
